403e837e6de80979c8ff8916b561b53062ae2072
sujan <dev.mern.sujan@gmail.com>
2022-08-29 12:11:34 +0600

    icon set up korlam

--- src/helpers/accordian/Accordian.jsx
+++ src/helpers/accordian/Accordian.jsx
@@ -10,9 +10,21 @@ const Accordian = ({ open, handleOpen, index, heading, details }) => {
       <h2>
         <span className='number'>
           {index + 1 < 10 ? `0${index + 1}` : index + 1}
-          {/* {index + 1 < 10 ? `0${index + 1}` : index + 1} */}
         </span>
+
         {heading}
+
+        <span className='icon'>
+          {open
+          ?
+            <i class="fa-solid fa-xmark"></i>
+          :
+            <i class="fa-solid fa-plus"></i>
+          }
+          
+          
+        </span>
+       
       </h2>
       
       {open && 
