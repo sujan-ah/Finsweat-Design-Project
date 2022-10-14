6d4af98d3cfd88786555a5f1fe51d72ce04623db
sujan <dev.mern.sujan@gmail.com>
2022-10-14 12:00:01 +0600

    navbar & footer part all part e add && pricing part design done

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-        <div class="container">
-            <div class="box">
-                <div class="logo">
-                    <img src="images/logo.png" alt="logo.jpg"/>
-                </div>
-                <div class="nav-item">
-                    <ul>
-                        <li>Home</li>
-                        <li>About us</li>
-                        <li>Features</li>
-                        <li>Pricing</li>
-                        <li>FAQ</li>
-                        <li>Blog</li>
-                    </ul>
-                    <a class="button">
-                        Contact us
-                    </a>
-                </div>
-            </div>
+      <div class="container">
+        <div class="box">
+          <div class="logo">
+            <img src="images/logo.png" alt="logo.jpg" />
+          </div>
+          <div class="nav-item">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About us</Link>
+              </li>
+              <li>Features</li>
+              <li>
+                <Link to="/pricing">Pricing</Link>
+              </li>
+              <li>FAQ</li>
+              <li>Blog</li>
+            </ul>
+            <a class="button">Contact us</a>
+          </div>
         </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
